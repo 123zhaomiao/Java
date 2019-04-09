70a0ec1aaaa2544e84987a5ea60d302072437942
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-09 17:00:31 +0800

    懒汉式单例Double check

---
+++ Java中的单例模式3.java
@@ -0,0 +1,22 @@
+
+class Singleton{
+    private static Singleton singleton;
+    //构造方法私有化、类外部创建不了对象
+    private  Singleton(){}
+    public static  Singleton getInstance() {
+        if(singleton == null){
+            synchronized (Singleton.class){
+                singleton = new Singleton();
+            }
+        }
+        return singleton;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Singleton singleton = Singleton.getInstance();
+        Singleton singleton1 = Singleton.getInstance();
+        // == 比较的是singleton与singleton1的地址
+        System.out.println(singleton == singleton1);
+    }
+}
