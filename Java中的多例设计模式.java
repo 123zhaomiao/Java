f158ad84d1128b58fc9a8135f919beca44e1fa6b
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-09 17:01:16 +0800

    多例设计模式

---
+++ Java中的多例设计模式.java
@@ -0,0 +1,34 @@
+class Sex{
+    private String name;
+
+    public Sex(String name) {
+        this.name = name;
+    }
+
+    private static Sex male = new Sex("男");
+    private static Sex female = new Sex("女");
+
+    public static Sex getInstance(String name){
+        if(name.equals("male")){
+            return male;
+        }else if(name.equals("female")){
+            return female;
+        }else{
+            return null;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
+public class Test{
+    public static void main(String[] args) {
+        Sex male = Sex.getInstance("male");
+        Sex female = Sex.getInstance("female");
+        System.out.println(male);
+        System.out.println(female);
+        System.out.println(male == female);
+    }
+}
