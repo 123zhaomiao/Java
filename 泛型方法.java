6c27ec8ca3dc6ce8fd5ca957e50734bd7d08770e
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-17 21:01:34 +0800

    Create 泛型方法.java

---
+++ 泛型方法.java
@@ -0,0 +1,19 @@
+class Myclass{
+    public <T> void print(T t){
+        System.out.println(t);
+    }
+    public <T> T print1(T t){
+        return t;
+    }
+}
+public class Test{
+    public static void main(String[] args) {
+        Myclass myClass = new Myclass();
+        myClass.print("hello");
+        myClass.print(12);
+        myClass.print(1.2);
+        System.out.println(myClass.print1("hello"));
+        System.out.println(myClass.print1(12));
+        System.out.println(myClass.print1(1.2));
+    }
+}
