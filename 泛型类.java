7399cad5582b426f917a7bd98adeb85d4b53e3e3
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-17 21:00:17 +0800

    Create 泛型类.java

---
+++ 泛型类.java
@@ -0,0 +1,40 @@
+class Point<T>{
+   private T x;
+   private T y;
+    /**
+     * 这不叫泛型方法，只是返回值是一个泛型
+     * @return
+     */
+    public T getX() {
+        return x;
+    }
+
+    public void setX(T x) {
+        this.x = x;
+    }
+
+    public T getY() {
+        return y;
+    }
+
+    public void setY(T y) {
+        this.y = y;
+    }
+}
+public class Test{
+    public static void main(String[] args) {
+        Point<Integer> point = new Point<>();
+        point.setX(10);
+        point.setY(20);
+        int x = point.getX();
+        int y  = point.getY();
+        System.out.println(x+"、"+y);
+
+        Point<String> point1 = new Point<>();
+        point1.setX("好好学习");
+        point1.setY("天天向上");
+        String x1 = point1.getX();
+        String x2 = point1.getY();
+        System.out.println(x1+"、"+x2);
+    }
+}
