62114890a8498b466bd5487b5db5f751f4550d37
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-11 18:17:03 +0800

    toString

---
+++ Object类/toString.java
@@ -0,0 +1,42 @@
+class Person{
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        Person per = new Person("张三",20);
+        String str = "hello";
+        fun(per);
+        fun(str);
+    }
+    private static void fun(Object obj){
+        System.out.println(obj.toString());
+        System.out.println(obj);
+    }
+}
+--------------------- 
+作者：zhao_miao 
+来源：CSDN 
+原文：https://blog.csdn.net/zhao_miao/article/details/89033283 
+版权声明：本文为博主原创文章，转载请附上博文链接！
