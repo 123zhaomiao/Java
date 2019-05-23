5352e2da095d23ea84bd8506a849f06b8918afa9
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-23 21:27:07 +0800

    Add files via upload

---
+++ UserInfo.java
@@ -0,0 +1,37 @@
+package vo;
+
+/**
+ * 用户信息类
+ */
+public class UserInfo {
+    private String username;
+    private String password;
+
+    public UserInfo(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfo{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
