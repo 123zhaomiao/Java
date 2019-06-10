0526e9ba2dc7cf0168fc3a2636fd4bcaeae98890
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-10 23:40:24 +0800

    toString

--- Object类/toString.java
+++ Object类/toString.java
@@ -21,12 +21,14 @@ class Person{
     }
 }
 public class Test {
+    
     public static void main(String[] args) {
         Person per = new Person("张三",20);
         String str = "hello";
         fun(per);
         fun(str);
     }
+    
     private static void fun(Object obj){
         System.out.println(obj.toString());
         System.out.println(obj);
