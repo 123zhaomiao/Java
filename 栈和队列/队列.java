2671fd353f27c7a4ae4a8f980dee176f331dd28a
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-10 22:25:54 +0800

    队列

--- 栈和队列/队列.java
+++ 栈和队列/队列.java
@@ -1,6 +1,7 @@
 import java.util.LinkedList;
 import java.util.Queue;
 public class Test {
+    
     public static void main(String[] args) {
         //1.创建一个队列
         Queue<String> queue = new LinkedList<>();
