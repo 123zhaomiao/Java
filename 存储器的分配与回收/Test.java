77c8684ca0ca51f95b494d7a58efbe2a1b0c96c0
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-19 23:21:52 +0800

    Test.java

--- 存储器的分配与回收/Test.java
+++ 存储器的分配与回收/Test.java
@@ -11,11 +11,9 @@ class Works
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public int getSize() {
         return size;
     }
